09a2c752633fefb725482668c009f20be2855af0
fortniteyi939-sys <fortniteyi939@gmail.com>
2026-04-15 16:24:10 -0500

    Arreglo

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const agregarTarea = () => {
+
+    const input = document.getElementById('tarea');
+    const textoTarea = input.value;
+    const nuevaTarea = document.createElement('li');
+    nuevaTarea.textContent = textoTarea;
+    nuevaTarea.onclick = function() {
+  
+      this.remove();
+  
+    };
+
+    document.getElementById('lista').appendChild(nuevaTarea);
+    input.value = "";
+  };
+
 li.onclick = function() {
     this.remove();
 }
